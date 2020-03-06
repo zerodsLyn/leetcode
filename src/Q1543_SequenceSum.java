027bcd42d5f9d135e69d06fa24ecf4b6b1063097
gengchao05 <gengchao05@meituan.com>
2020-03-06 19:11:43 +0800

    1>swap nodes in pairs
    2>sequence sum 2 target
    
    3>merge a short sorted array to a long sortedarray

---
+++ src/Q1543_SequenceSum.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/
+ * @author gengchao05 create on 2020/03/06
+ */
+public class Q1543_SequenceSum {
+    public int[][] findContinuousSequence(int target) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+        int mid;
+        if (target % 2 == 1) {
+            mid = target / 2 + 1;
+        } else {
+            mid = target / 2;
+        }
+
+        int lo = 1;
+        int hi = 2;
+        while (lo < hi && hi <= mid) {
+            int sum = (lo + hi) * (hi - lo + 1) / 2;
+            if (sum == target) {
+                ArrayList<Integer> seq = new ArrayList<>();
+                for (int i = lo; i <= hi; i++) {
+                    seq.add(i);
+                }
+                result.add(seq);
+                lo++;
+            } else if (sum > target) {
+                lo++;
+            } else {
+                hi++;
+            }
+        }
+
+        int[][] arrayResult = new int[result.size()][];
+        for (int i = 0; i < result.size(); i++) {
+            ArrayList<Integer> integers = result.get(i);
+            int[] seq = new int[integers.size()];
+            for (int j = 0; j < integers.size(); j++) {
+                seq[j] = integers.get(j);
+            }
+            arrayResult[i] = seq;
+        }
+        return arrayResult;
+    }
+
+    public static void main(String[] args) {
+        int[][] continuousSequence = new Q1543_SequenceSum().findContinuousSequence(9);
+        for (int[] ints : continuousSequence) {
+            System.out.println(Arrays.toString(ints));
+        }
+    }
+}
---
+++ src/Q24_SwapNodesInPairs.java
@@ -0,0 +1,66 @@
+/**
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ * @author gengchao05 create on 2020/03/06
+ */
+public class Q24_SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        int even = 1;
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode next = head.next;
+
+        ListNode tmp;
+        while (next != null) {
+            if (even == 1) {
+                if (prev == null) {
+                    head = next;
+                    tmp = next.next;
+                    next.next = cur;
+                    cur.next = tmp;
+
+                    tmp =next.next;
+                    prev = cur;
+                    cur = next;
+                    next = tmp;
+                } else {
+                    tmp = next.next;
+                    prev.next = next;
+                    next.next = cur;
+                    cur.next = tmp;
+
+                    tmp =next.next;
+                    prev = cur;
+                    cur = next;
+                    next = tmp;
+
+                }
+            }
+
+            prev = cur;
+            cur = next;
+            next = cur.next;
+            even = 1 - even;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head1 = new ListNode(1);
+        ListNode head2 = new ListNode(2);
+        ListNode head3 = new ListNode(3);
+        ListNode head4 = new ListNode(4);
+        head1.next = head2;
+        head2.next = head3;
+        head3.next = head4;
+        head4.next = null;
+
+        System.out.println(new Q24_SwapNodesInPairs().swapPairs(head1));
+
+    }
+}
