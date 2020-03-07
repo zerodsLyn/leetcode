5e4ada05f728424d2e8936404f9b82cc70174108
gengchao05 <gengchao05@meituan.com>
2020-03-08 00:41:08 +0800

    parentheses and remove duplicates from sorted array

---
+++ src/Q22_GenerateParentheses_1.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author gengchao05 create on 2019/05/08
+ */
+public class Q22_GenerateParentheses_1 {
+    List<String> result = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        if (n == 0) {
+            return result;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        track(n, sb, 0, 0);
+        return result;
+    }
+
+    public void track(int n, StringBuilder sb, int left, int right) {
+        if (sb.toString().length() == 2 * n) {
+            result.add(sb.toString());
+            return;
+        }
+
+        if (left == right) {
+            sb.append("(");
+            track(n, sb, left + 1, right);
+            sb.deleteCharAt(sb.length() - 1);
+        } else if (left == n){
+            sb.append(")");
+            track(n, sb, left, right + 1);
+            sb.deleteCharAt(sb.length() - 1);
+        } else {
+            sb.append("(");
+            track(n, sb, left + 1, right);
+            sb.deleteCharAt(sb.length() - 1);
+
+            sb.append(")");
+            track(n, sb, left, right + 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Q22_GenerateParentheses_1().generateParenthesis(3));
+    }
+}
---
+++ src/Q26_RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+
+/**
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+ * @author gengchao05 create on 2020/03/07
+ */
+public class Q26_RemoveDuplicatesFromSortedArray {
+
+
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int length = nums.length;
+        if (nums.length == 1){
+            return 1;
+        }
+
+        int deleteNum = 0;
+
+        int curVal = nums[nums.length - 1];
+        int curIndex = nums.length - 2;
+        while (curIndex >= 0) {
+            if (nums[curIndex] == curVal) {
+                for (int i = curIndex; i < nums.length - deleteNum - 1; i++) {
+                    nums[i] = nums[i + 1];
+                }
+                deleteNum++;
+            } else  {
+                curVal = nums[curIndex];
+            }
+            curIndex--;
+        }
+
+        return length - deleteNum;
+    }
+
+    public int removeDuplicates1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (nums.length == 1){
+            return 1;
+        }
+
+        int curDiffNum = 1;
+        int num = nums[0];
+        int j = 1;
+        while (j < nums.length) {
+            if (nums[j] == num) {
+                j++;
+            } else {
+                nums[curDiffNum++] = nums[j];
+                num = nums[j++];
+            }
+        }
+
+        return curDiffNum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,2,3};
+        System.out.println(new Q26_RemoveDuplicatesFromSortedArray().removeDuplicates1(nums));
+        System.out.println(Arrays.toString(nums));
+
+        int[] nums1 = new int[] {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(new Q26_RemoveDuplicatesFromSortedArray().removeDuplicates1(nums1));
+        System.out.println(Arrays.toString(nums1));
+
+        int[] nums2 = new int[] {1,1,2};
+        System.out.println(new Q26_RemoveDuplicatesFromSortedArray().removeDuplicates1(nums2));
+        System.out.println(Arrays.toString(nums2));
+    }
+}
