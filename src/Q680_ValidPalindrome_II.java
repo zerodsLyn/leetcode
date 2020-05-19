0656d5a6dbd4398f75bdef2cb41bac5a9b91c8e4
zerodsLyn <zerodseu@gmail.com>
2020-05-19 23:21:00 +0800

    double point valid palindrome

---
+++ src/Q680_ValidPalindrome_II.java
@@ -0,0 +1,48 @@
+/**
+ * @author zerodsLyn
+ * created on 2020/5/19
+ */
+public class Q680_ValidPalindrome_II {
+
+    public boolean validPalindrome(String s) {
+        if (s.length() < 3) return true;
+
+        int l = 0, r = s.length() - 1;
+        boolean flag = true;
+        while (l < r) {
+            char c_l = s.charAt(l);
+            char c_r = s.charAt(r);
+            if (c_l == c_r) {
+                l++;
+                r--;
+            } else {
+                return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            char c_l = s.charAt(left);
+            char c_r = s.charAt(right);
+            if (c_l == c_r) {
+                left += 1;
+                right -= 1;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Q680_ValidPalindrome_II().validPalindrome("aba"));
+        System.out.println(new Q680_ValidPalindrome_II().validPalindrome("abca"));
+        System.out.println(new Q680_ValidPalindrome_II().validPalindrome("adbca"));
+        System.out.println(new Q680_ValidPalindrome_II().validPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"));
+        System.out.println(new Q680_ValidPalindrome_II().validPalindrome("pidbliassaqozokmtgahluruufwbjdtayuhbxwoicviygilgzduudzgligyviciowxbhuyatdjbwfuurulhagtmkozoqassailbdip"));
+    }
+}
