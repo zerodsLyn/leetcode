83958c94e38777507b3c3f624e9bfbad1a8b6365
zerodsLyn <zerodseu@gmail.com>
2020-05-21 00:09:45 +0800

    valid parentheses

--- src/Q20_ValidParentheses.java
+++ src/Q20_ValidParentheses.java
@@ -1,44 +1,39 @@
-/**
- * @author zerodsLyn create on 2019/06/26
- */
-
-import java.util.Map;
 import java.util.Stack;
 
 /**
  * https://leetcode-cn.com/problems/valid-parentheses/
+ * @author zerodsLyn create on 2019/06/26
  */
 public class Q20_ValidParentheses {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
-        for (int i = 0; i < s.length(); i++) {
+        Stack<Character> chars = new Stack<>();
+        int len = s.length();
+        if (len == 0) return true;
+        if (len % 2 == 1) return false;
+
+        chars.push(s.charAt(0));
+        for (int i = 1; i < len; i++) {
             char c = s.charAt(i);
-            if ('(' == c || '[' == c || '{' == c) {
-                stack.push(c);
+            if (c == '(' || c == '[' || c == '{') {
+                chars.push(c);
             } else {
-                Character peek = stack.isEmpty() ? ' ' : stack.peek();
-                if (')' == c) {
-                    if (peek == '(') {
-                        stack.pop();
-                    } else {
-                        return false;
-                    }
-                } else if (']' == c) {
-                    if (peek == '[') {
-                        stack.pop();
-                    } else {
-                        return false;
-                    }
-                } else if ('}' == c) {
-                    if (peek == '{') {
-                        stack.pop();
-                    } else {
-                        return false;
-                    }
+                if (chars.size() == 0) return false;
+
+                char peek = chars.peek();
+                if (c == ')') {
+                    if (peek == '(') chars.pop();
+                    else return false;
+                } else if (c == ']') {
+                    if (peek == '[') chars.pop();
+                    else return false;
+                } else {
+                    if (peek == '{') chars.pop();
+                    else return false;
                 }
             }
         }
-        return stack.isEmpty();
+
+        return chars.size() == 0;
     }
 
     public static void main(String[] args) {
