e17166cac92c6b4aed4f6e652c0796073ffdf73d
zerodsLyn <zerodseu@gmail.com>
2020-07-13 20:02:48 +0800

    Q350_IntersectionOfTwoArrays

---
+++ src/array/Q350_IntersectionOfTwoArrays.java
@@ -0,0 +1,36 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author gengchao05 create on 2020/07/13
+ */
+public class Q350_IntersectionOfTwoArrays {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int size1 = nums1.length, size2 = nums2.length;
+
+        List<Integer> result = new ArrayList<>();
+        int i1 = 0, i2 = 0;
+        while (i1 < size1 && i2 < size2) {
+            if (nums1[i1] == nums2[i2]) {
+                result.add(nums1[i1]);
+                i1++;
+                i2++;
+            } else if (nums1[i1] < nums2[i2]) {
+                i1++;
+            } else if (nums1[i1] > nums2[i2]) {
+                i2++;
+            }
+        }
+
+        int[] interNums = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            interNums[i] = result.get(i);
+        }
+        return interNums;
+    }
+}
