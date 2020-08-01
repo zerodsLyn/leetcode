b52da2cd9f7c96ecb7f83c8d35f9a73ee1cef981
zerodsLyn <zerodseu@gmail.com>
2020-08-02 00:27:19 +0800

    zigzag print

--- src/tree/Q_ZigzagLevelOrder.java
+++ src/tree/Q_ZigzagLevelOrder.java
@@ -53,4 +53,49 @@ public class Q_ZigzagLevelOrder {
 
         return result;
     }
+
+    public List<List<Integer>> zigzagLevelOrder2(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (root == null) return result;
+
+        LinkedList<TreeNode> curNodes = new LinkedList<>();
+        curNodes.add(root);
+        boolean reverse = false;
+        Stack<TreeNode> stack = new Stack<>();
+        while (curNodes.size() > 0) {
+            int size = curNodes.size();
+            List<Integer> singleRow = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = curNodes.poll();
+                if (reverse) {
+                    stack.push(node);
+                    if (i == size - 1) {
+                        while (!stack.isEmpty()) {
+                            singleRow.add(stack.pop().val);
+                        }
+                    }
+                } else {
+                    singleRow.add(node.val);
+                }
+                if (node.left != null) curNodes.offer(node.left);
+                if (node.right != null) curNodes.offer(node.right);
+            }
+            reverse = !reverse;
+            result.add(singleRow);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+        System.out.println(new Q_ZigzagLevelOrder().zigzagLevelOrder2(node1));
+    }
 }
