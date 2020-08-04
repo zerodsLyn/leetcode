7b7448ed9a5767b27756d73a6b749ab46c4c6fab
zerodsLyn <zerodseu@gmail.com>
2020-08-04 21:11:56 +0800

    tree symmetric and lowest common ancester

--- src/tree/Q101_SymmetricTree.java
+++ src/tree/Q101_SymmetricTree.java
@@ -8,14 +8,15 @@ package tree;
 public class Q101_SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
-        return isSymmetric(root.left, root.right);
+        if (root.left == null && root.right == null) return true;
+        if (root.left != null && root.right != null) return isSymmetric(root.left, root.right);
+        return false;
     }
 
-    private boolean isSymmetric(TreeNode left, TreeNode right) {
-        if (left == null && right == null) return true;
-        if (left == null && right != null) return false;
-        if (right == null && left != null) return false;
-        if (left.val != right.val) return false;
-        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left != null && right != null) {
+            return left.val == right.val && isSymmetric(left.right, right.left) && isSymmetric(left.left, right.right);
+        }
+        return left == null && right == null;
     }
 }
--- src/tree/Q236_LowestCommonAncester.java
+++ src/tree/Q236_LowestCommonAncester.java
@@ -7,19 +7,10 @@ package tree;
 public class Q236_LowestCommonAncester {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) return root;
-        TreeNode left = lowestCommonAncestor(root.left,p,q);
-        TreeNode right = lowestCommonAncestor(root.right,p,q);
-        // 如果在一边，必定有一个返回null
-        // 如果不在一遍，那么必定都不为空
-        if (left != null && right != null) {
-            return root;
-        } else if (left == null) {
-            return right;
-        } else if (right == null) {
-            return left;
-        } else {
-            // 这种情况不会走到
-            return null;
-        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        if (left == null) return right;
+        return left;
     }
 }
