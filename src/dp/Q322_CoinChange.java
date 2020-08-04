952b9f1ddeb1fa16c9e9ef10759ddef035f40526
zerodsLyn <zerodseu@gmail.com>
2020-08-04 21:33:34 +0800

    coinChange

--- src/dp/Q322_CoinChange.java
+++ src/dp/Q322_CoinChange.java
@@ -6,17 +6,14 @@ package dp;
  */
 public class Q322_CoinChange {
     public int coinChange(int[] coins, int amount) {
+        int len = coins.length;
         int[] dp = new int[amount + 1];
-        for (int i = 1; i <= amount; i++) {
-            dp[i] = Integer.MAX_VALUE;
-        }
-
+        for (int i = 1; i <= amount; i++) dp[i] = Integer.MAX_VALUE;
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
-                if (i - coin < 0 || dp[i - coin] == Integer.MAX_VALUE) {
-                    continue;
-                } else {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                int prevI = i - coin;
+                if (prevI >= 0 && dp[prevI] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[prevI] + 1);
                 }
             }
         }
