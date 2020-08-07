cdbd687d08ac572347437afe416095b108997ae2
zerodsLyn <zerodseu@gmail.com>
2020-08-07 08:08:51 +0800

    is same tree

---
+++ src/tree/Q100_IsSameTree.java
@@ -0,0 +1,16 @@
+package tree;
+
+/**
+ * @author zerodsLyn
+ * created on 2020/8/7
+ */
+public class Q100_IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        else if (q == null) return false;
+        else if (p == null) return false;
+
+        return p.val == q.val && isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+    }
+}
